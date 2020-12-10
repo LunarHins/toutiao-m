d19944bd24e57ff888966e9b76a30a629af0f0b1
dlo <Dloading@Russell.com>
2020-12-11 00:32:23 +0800

    修改

--- toutiao-m/src/main.js
+++ toutiao-m/src/main.js
@@ -1,10 +1,21 @@
+
+
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import  './styles/index.less'
+
 Vue.config.productionTip = false
 
+
+
+//new一个vue实例，把router等包配置到根实例中
+// 通过render方法，把App根组件渲染到#app节点中。
+//找到index.html中id为app的div
+//index.html就是我们SPA单页面应用程序中的单页面。
 new Vue({
   router,
   store,
--- toutiao-m/src/router/index.js
+++ toutiao-m/src/router/index.js
@@ -1,27 +1,21 @@
+//路由模块
+/* eslint-disable */
+//路由模块
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  //配了两个路由表
+  
 ]
 
+//创建路由实例
 const router = new VueRouter({
   routes
 })
-
+//导出/暴露路由实例。
 export default router
